4311cd7e95c7f52695d48ea15cfd6841bedf5fa0
Bogdan <Bogdandz@xxx>
2025-09-21 15:17:14 +0500

    Добавлены изменения в 15:16C:/Users/user/AppData/Local/Programs/Python/Python313/python.exe

--- some_file.py
+++ some_file.py
@@ -1,1 +1,11 @@
 print("првиет с удаленки")
+array = []
+for x in range(11):
+    array.append(x)
+print(array)
+print("Добавил изменения в 15:13")
+print("ПРивет")
+array = []
+for x in range(11):
+    array.append(x)
+print(array)
